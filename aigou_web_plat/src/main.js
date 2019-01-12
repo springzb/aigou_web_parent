dab230ca4273dd8b508263cfae22fdaa8c9303e4
admin <admin@qq.com>
2019-01-12 16:15:48 +0800

    初始化

---
+++ aigou_web_plat/src/main.js
@@ -0,0 +1,61 @@
+import babelpolyfill from 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+import VueRouter from 'vue-router'
+import store from './vuex/store'
+import Vuex from 'vuex'
+import routes from './routes'
+//import Mock from './mock' //自己mock
+//Mock.bootstrap();
+import 'font-awesome/css/font-awesome.min.css'
+Vue.use(ElementUI)
+Vue.use(VueRouter)
+Vue.use(Vuex)
+
+//NProgress.configure({ showSpinner: false });
+//对axios进行统一配置
+import axios from 'axios' //和引入vue一样
+//var axios = require('axios')
+//axios.defaults.baseURL = "https://www.easy-mock.com/mock/5c3957f10157fc56d707bc95/aigou/services"//配置前缀 easymock
+axios.defaults.baseURL = "http://127.0.0.1:9527/services"//配置前缀
+// 将API方法绑定到全局  /plat/login
+Vue.prototype.$http = axios //给Vue这个类添加一个原型的属性,这个类的对象都能调用
+Vue.config.productionTip = false
+const router = new VueRouter({
+  routes
+})
+
+//路由每次执行前,都会判断是否登录
+router.beforeEach((to, from, next) => {
+  //NProgress.start();
+  if (to.path == '/login') {
+    //sessionStorage 前端中session
+    sessionStorage.removeItem('user');
+  }
+
+  //c从session获取用户
+  let user = JSON.parse(sessionStorage.getItem('user'));
+
+  //null表示false !null=true
+  if (!user && to.path != '/login') {
+    next({ path: '/login' })
+  } else {
+    next()
+  }
+})
+
+//router.afterEach(transition => {
+//NProgress.done();
+//});
+
+new Vue({
+  //el: '#app',
+  //template: '<App/>',
+  router,
+  store,
+  //components: { App }
+  render: h => h(App)
+}).$mount('#app')
+
---
+++ aigou_web_plat/src/routes.js
@@ -0,0 +1,76 @@
+import Login from './views/Login.vue'
+import NotFound from './views/404.vue'
+import Home from './views/Home.vue'
+import Main from './views/Main.vue'
+import Table from './views/nav1/Table.vue'
+import Form from './views/nav1/Form.vue'
+import user from './views/nav1/user.vue'
+import Page4 from './views/nav2/Page4.vue'
+import Page5 from './views/nav2/Page5.vue'
+import Page6 from './views/nav3/Page6.vue'
+import echarts from './views/charts/echarts.vue'
+
+let routes = [
+    {
+        path: '/login',
+        component: Login,
+        name: '',
+        hidden: true
+    },
+    {
+        path: '/404',
+        component: NotFound,
+        name: '',
+        hidden: true
+    },
+    {
+        path: '/',
+        component: Home,
+        name: '首页',
+        leaf: true,//只有一个节点
+        iconCls: 'fa fa-bar-chart',
+        redirect: { path: '/main' },
+        children: [
+            { path: '/main', component: echarts, name: '首页' }
+        ]
+    },
+    {
+        path: '/',
+        component: Home,
+        name: '商品模块',
+        iconCls: 'el-icon-message',//图标样式class
+        children: [
+            { path: '/productBrand', component: Main, name: '商品品牌' },
+            { path: '/productType', component: Table, name: '商品类型' },
+            { path: '/productTemplate', component: Form, name: '商品模板' },
+            { path: '/productViewProperties', component: user, name: '商品规格' },
+            { path: '/product', component: Page4, name: '商品管理' },
+        ]
+    },
+    {
+        path: '/',
+        component: Home,
+        name: '系统管理',
+        iconCls: 'fa fa-id-card-o',
+        children: [
+            { path: '/page4', component: Page4, name: '员工管理' },
+            { path: '/page5', component: Page5, name: '部门管理' }
+        ]
+    },
+    {
+        path: '/',
+        component: Home,
+        name: '营销管理',
+        iconCls: 'fa fa-address-card',
+        children: [
+            { path: '/page6', component: Page6, name: '营销活动管理' }
+        ]
+    },
+    {
+        path: '*',
+        hidden: true,
+        redirect: { path: '/404' }
+    }
+];
+
+export default routes;
